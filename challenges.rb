5809ba3e47d55e36aaadc29f521165dc945020e3
longpatrick <long.patrick1997@gmail.com>
2021-10-01 12:06:07 -0500

    .

--- challenges.rb
+++ challenges.rb
@@ -1,16 +1,16 @@
-puts 'hello'
+# puts 'hello'
 
-(1..5).each do |iteration| 
-    puts "Iteration #{iteration}"
-end
+# (1..5).each do |iteration| 
+#     puts "Iteration #{iteration}"
+# end
 
 my_array = [1, 2, 4, 6, 4, 7, 4, 3, 5]
 
-puts my_array.pop
-p my_array
-puts my_array.shift
-p my_array
-puts my_array.delete_at(0)
-p my_array
-puts my_array.delete(4)
-p my_array
+# puts my_array.pop
+# p my_array
+# puts my_array.shift
+# p my_array
+# puts my_array.delete_at(0)
+# p my_array
+# puts my_array.delete(4)
+# p my_array
