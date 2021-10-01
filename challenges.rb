970f85a4bac7d496c30b5790cc8b03eebe6e3adf
longpatrick <long.patrick1997@gmail.com>
2021-10-01 12:28:08 -0500

    write a number_to_string method to convert integers into strings

--- challenges.rb
+++ challenges.rb
@@ -14,3 +14,11 @@ my_array = [1, 2, 4, 6, 4, 7, 4, 3, 5]
 # p my_array
 # puts my_array.delete(4)
 # p my_array
+
+
+# Convert a number to a string method
+def number_to_string (num) 
+    p num.to_s
+end 
+
+number_to_string(5);
